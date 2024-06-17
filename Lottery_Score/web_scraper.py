f9ffba4b44a3707c763b263002c6a337e56218ef
RobisonTorres <robison.torres21@gmail.com>
2024-06-17 16:34:05 -0300

    update code.

--- Lottery_Score/web_scraper.py
+++ Lottery_Score/web_scraper.py
@@ -1,31 +1,38 @@
-import requests, bs4, re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import re
 
-# Not working.
 def web_search(round):
 
-    # This function accesses the lottery's page and takes the html.
-    url = f'https://www.intersena.com.br/lotofacil/resultados/{round}'
-    source = requests.get(url).text
-    soup = bs4.BeautifulSoup(source, 'html.parser')
-    return soup
+    # This function accesses the lottery's page and takes the result.
+    driver = webdriver.Chrome()
+    driver.get("https://www.intersena.com.br/lotofacil/resultados")
+
+    # Input the round, clink the button and get the result.
+    search = driver.find_element(By.ID, "inputNumeroConcurso")
+    search.send_keys(round)
+    click_button = driver.find_element(By.XPATH, "//*[contains(text(), 'PESQUISAR')]").click()
+    result = driver.find_element(By.CLASS_NAME, "table-responsive").text
+
+    # Close the browser.
+    driver.close()
+    
+    return result
 
 def extract_search(round=False):
 
     # This function extracts only the lottery result from 
     # the web_search, and gives a proper format to the result.
     if round == False: round = input('What round do you want to check: ')
-    
-    target = web_search(round)
-    round_found = target.find('h1', class_='col-md-6 col-sm-6 col-xs-12')  
-    lottery_numbers = target.find('div', class_='resultado-individual-sorteio')  
-    
-    # Only numbers.
-    round_found = re.sub('[^0-9]', '', round_found.text)
-    lottery_numbers = re.sub('[^0-9]', '', lottery_numbers.text)
-      
-    if str(round) != round_found: 
-        return f'\nRound: {round} - Result not found.\n'
 
-    else:
-        lottery_result = [int(lottery_numbers[x:x+2]) for x in range(0, len(lottery_numbers), 2)]
-        return [lottery_result, round]
+    # Perform the web search.
+    lottery_numbers = web_search(round)
+
+    # Convert the formatted string into a list of integers (each representing a lottery number)
+    lottery_numbers = re.findall(r'(\d{2}(?:-\d{2}){14})', lottery_numbers)   
+    lottery_numbers = re.sub('[^0-9]', '', lottery_numbers[0])
+    lottery_result = [int(lottery_numbers[x:x+2]) for x in range(0, len(lottery_numbers), 2)]
+    
+    return [lottery_result, round]
+
+print(extract_search())
