24f11af267d7133260184029aa9e0361f11d87b1
RobisonTorres <robison.torres21@gmail.com>
2024-06-17 18:08:27 -0300

    first commit.

---
+++ Youtube_Downloader/main.py
@@ -0,0 +1,14 @@
+from youtube_downloader import download_main_video
+from youtube_audio import download_main_audio
+
+def main():
+    
+    choice = input('Press 1 to download videos or 2 to only audios: ')
+    if choice == '1':
+        print(download_main_video())
+    elif choice == '2':
+        print(download_main_audio())
+    else:
+        print('wrong option!')
+
+main()
