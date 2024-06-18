8a6f0fe4d249dd0ae85c6c16152fa83db84e757d
RobisonTorres <robison.torres21@gmail.com>
2024-06-17 22:15:00 -0300

    updated code.

--- Youtube_Downloader/main.py
+++ Youtube_Downloader/main.py
@@ -1,14 +1,48 @@
-from youtube_downloader import download_main_video
-from youtube_audio import download_main_audio
+import youtube_downloader
+from pathlib import Path
+print('Youtube Downloader.')
+print()
 
-def main():
+def directory_path():
     
-    choice = input('Press 1 to download videos or 2 to download only audios: ')
-    if choice == '1':
-        print(download_main_video())
-    elif choice == '2':
-        print(download_main_audio())
-    else:
-        print('wrong option!')
+    # This function determines the download's location.
+    directory = (input(r"Type in the directory's path or hit enter to choose the 'Downloads' folder: "))
+    downloads = str(Path.home() / "Downloads")
+    return directory or downloads
 
-main()
+def repeat():
+
+    # This function repeats the operation if asked.
+    while True:
+        option = input('\nPress "y" to continue downloading: ').lower()
+        if option != 'y':
+            break  
+        download()
+
+def download():
+
+    # This function downloads audios or playlists from youtube based on user's input.
+    directory = directory_path()
+    option = input('Press 1 to download video or 2 to download audio: ')
+
+    if option == '1':
+        video = input('\nPress v to download video or p to download playlist: ').lower()
+        if video == 'v':
+            youtube_downloader.download_video(directory, link = input("Type in the videos's url: "))
+        elif video == 'p':
+            youtube_downloader.download_playlist(youtube_downloader.download_video, directory, link = input("Type in the playlist's url: "))
+        else:
+            print('Wrong option.')            
+    
+    elif option == '2':
+        audio = input('Press a to download audio or p to download playlist: ').lower()
+        if audio == 'a':
+            youtube_downloader.download_audio(directory, link = input("Type in the videos's url: "))
+        elif audio == 'p':
+            youtube_downloader.download_playlist(youtube_downloader.download_audio, directory, link = input("Type in the playlist's url: "))
+        else:
+            print('Wrong option.')   
+    repeat()
+    return 'Operation finished.'
+
+print(download())
--- Youtube_Downloader/youtube_downloader.py
+++ Youtube_Downloader/youtube_downloader.py
@@ -1,14 +1,6 @@
 from pytube import YouTube, Playlist
 from pathlib import Path
-print('Youtube Downloader.')
-print()
-
-def directory_path():
-    
-    # This function determines the download's location.
-    directory = (input(r"Type in the directory's path or hit enter to choose the 'Downloads' folder: "))
-    downloads = str(Path.home() / "Downloads")
-    return directory or downloads
+import os
 
 def download_video(directory, link):
     
@@ -20,34 +12,24 @@ def download_video(directory, link):
         print('Download successfully.')
     except: return 'Sorry, something went wrong.'
 
-def download_playlist(directory, link):
+def download_audio(directory, link):
+    
+    # This function downloads videos from YouTube on format .mp3 and stores on 'Downloads' folder.
+    youtube_video = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+    youtube_video = youtube_video.streams.get_audio_only()
+    try:
+        out_file = youtube_video.download(directory)
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        print('Download successfully.')
+    except: return 'Download successfully.'
+
+def download_playlist(function, directory, link):
 
     # This function downloads playlists form YouTube.
     playlist = Playlist(link)
     playlist_videos = list(playlist.video_urls)
     for video in playlist_videos:
-        download_video(directory, video)
+        function(directory, video)
     print('All videos has been downloaded.')
-
-def repeat():
-
-    # This function repeats the operation if asked.
-    while True:
-        option = input('\nPress "y" to continue downloading: ').lower()
-        if option != 'y':
-            break  
-        download_main_video()
-
-def download_main_video():
-
-    # This function downloads videos or playlists from youtube based on user's input.
-    directory = directory_path()
-    option = input('Press 1 to download video or 2 to download playlist: ')
-    if option == '1':
-        download_video(directory, link = input("Type in the videos's url: "))
-    elif option == '2':
-        download_playlist(directory, link = input("Type in the playlist's url: "))
-    else:
-        print('Wrong option.')
-    repeat()
-    return 'Operation finished.'
