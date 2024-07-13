91c2700871fa47f19cf28ee726fd717c147a076a
RobisonTorres <robison.torres21@gmail.com>
2024-07-13 14:47:04 -0300

    updated code.

--- Youtube_Downloader/main.py
+++ Youtube_Downloader/main.py
@@ -23,10 +23,10 @@ def download():
 
     # This function downloads audios or playlists from youtube based on user's input.
     directory = directory_path()
-    option = input('Press 1 to download video or 2 to download audio: ')
+    option = input('Press "1" to download video or "2" to download audio: ')
 
     if option == '1':
-        video = input('\nPress v to download video or p to download playlist: ').lower()
+        video = input('\nPress "v" to download video or "p" to download playlist: ').lower()
         if video == 'v':
             youtube_downloader.download_video(directory, link = input("Type in the videos's url: "))
         elif video == 'p':
@@ -35,13 +35,17 @@ def download():
             print('Wrong option.')            
     
     elif option == '2':
-        audio = input('Press a to download audio or p to download playlist: ').lower()
+        audio = input('Press "a" to download audio or "p" to download playlist: ').lower()
         if audio == 'a':
             youtube_downloader.download_audio(directory, link = input("Type in the videos's url: "))
         elif audio == 'p':
             youtube_downloader.download_playlist(youtube_downloader.download_audio, directory, link = input("Type in the playlist's url: "))
         else:
             print('Wrong option.')   
+
+    else:
+        print('Wrong option.')
+
     repeat()
     return 'Operation finished.'
 
